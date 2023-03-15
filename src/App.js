b51d3dcf41263f9890ce37580c4ce37063b88895
Dusan-Goll <d.goll@seznam.cz>
2023-03-15 17:58:05 +0100

    I created list of four types of goods.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>potraviny</h1>
+      <ul>
+        <li key="0" className="predmet-seznamu">
+          <h3>Paprika červená</h3>
+          <img
+            className="obrazek-zbozi"
+            src={require("./img/Paprika_cervena.jpg")}
+            alt="čerstvá červená paprika na dřevěném prkénku"
+            />
+        </li>
+
+        <li key="1" className="predmet-seznamu">
+        <h3>Paprika žlutá</h3>
+        <img
+            className="obrazek-zbozi"
+            src={require("./img/Paprika_zluta.jpg")}
+            alt="čerstvá žlutá paprika na dřevěném prkénku"
+              
+            />
+        </li>
+
+        <li key="1" className="predmet-seznamu">
+        <h3>Paprika zelená</h3>
+        <img
+            className="obrazek-zbozi"
+            src={require("./img/Paprika_zelena.jpg")}
+            alt="čerstvá zelená paprika na dřevěném prkénku"
+              
+            />
+        </li>
+
+        <li key="3" className="predmet-seznamu">
+        <h3>Česnek</h3>
+        <img
+            className="obrazek-zbozi"
+            src={require("./img/Cesnek.jpg")}
+            alt="palice suchého česneku na dřevěném prkénku"
+            />
+        </li>
+      </ul>
+    </>
   );
 }
 
