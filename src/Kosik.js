565ad70385ee5b69742ae86b858186fc0274c777
Dusan-Goll <d.goll@seznam.cz>
2023-03-20 09:29:09 +0100

    I added Kosik component.

---
+++ src/Kosik.js
@@ -0,0 +1,12 @@
+import './Kosik.css';
+
+export default function Kosik() {
+  return (
+    <section className="kosik">
+      <h2>Kosik</h2>
+      <ul>
+        <h4>V košíku nemáte žádné zboží.</h4>
+      </ul>
+    </section>
+  );
+}
