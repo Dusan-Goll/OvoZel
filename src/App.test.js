1afc292c1357e0c829180735a519089f97e78a7a
Dusan-Goll <d.goll@seznam.cz>
2023-03-29 13:11:00 +0200

    I tried some test.

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  test('App displays h1 with text "OvoZel"', () => {
+    render( <App/> );
+    expect(screen.getByText(/OvoZel/)).toBeInTheDocument();
+  });
 });
